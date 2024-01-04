ec2f1320ebaa8a03bd968631bed1d2e063396a9a
St <takundaveni@gmail.com>
2024-01-03 18:17:04 -0800

    9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    i = 0
+    i = int(abs(number) % 10)
+    if number > 0:
+        i = i
+    else:
+        i = i
+    print("{}".format(i), end="")
+    return (i)
