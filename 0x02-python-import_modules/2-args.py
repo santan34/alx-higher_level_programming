3df04fb697731d56755445596b129ce7ca3dd5b0
St <takundaveni@gmail.com>
2024-01-04 17:41:52 -0800

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv)
+    if l > 1:
+        string = "arguments"
+    else:
+        string = "argument"
+    for i in range(l):
+        if i == 0:
+            print("{} {}".format(l, string))
+        else:
+            print("{}: {}".format(i, sys.argv[i]))
