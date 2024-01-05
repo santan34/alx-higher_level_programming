5858ca01cedcb2a2b72ec942994455a982912d33
St <takundaveni@gmail.com>
2024-01-04 17:56:33 -0800

    2222

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = len(sys.argv)-1
-    if l > 1:
-        string = "arguments"
+    l = len(sys.argv)
+    if l > 2:
+        string = "arguments:"
+    elif l == 1:
+        string = "arguments."
     else:
-        string = "argument"
+        string = "argument:"
     for i in range(l):
         if i == 0:
-            print("{} {}:".format(l, string))
+            print("{} {}".format(l-1, string))
         else:
             print("{}: {}".format(i, sys.argv[i]))
