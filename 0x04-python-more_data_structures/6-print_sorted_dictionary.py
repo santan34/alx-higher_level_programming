edf57a27cb81d5f0edde087b314c9a82fa542819
St <takundaveni@gmail.com>
2024-01-10 15:49:33 -0800

    6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    new_dic = dict(sorted(a_dictionary.items(), key =lambda x: x[0]))
+    for i in new_dic:
+        print("{}: {}".format(i, new_dic[i]))
