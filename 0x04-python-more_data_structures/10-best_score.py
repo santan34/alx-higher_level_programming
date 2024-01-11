84ebb68efd3ce049867782453541c701c7673450
St <takundaveni@gmail.com>
2024-01-10 16:33:32 -0800

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+def best_score(a_dictionary):
+    a = 0
+    if a_dictionary is None:
+        return None
+    for i in a_dictionary:
+        if a < a_dictionary[i]:
+            a = a_dictionary[i]
+    return a
