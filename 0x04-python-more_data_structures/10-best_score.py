a74ab1036309d08fecf8a6fe49985084bf509162
St <takundaveni@gmail.com>
2024-01-10 16:45:54 -0800

    101010

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     a = 0
+    name = ""
     if a_dictionary is None:
         return None
     for i in a_dictionary:
         if a < a_dictionary[i]:
             a = a_dictionary[i]
-    return a
+            name = i
+    return name
