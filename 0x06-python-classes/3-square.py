6ca2ef510b774c1b851c62622062e25594734f9e
St <takundaveni@gmail.com>
2024-01-25 15:27:40 -0800

    33

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,8 +1,13 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
+"""
+working with the square class
+"""
+
+
 class Square:
     """
-    a class for square object
-    """
+       a class for square object
+       """
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -14,6 +19,28 @@ class Square:
         Args:
             size(int): size of the square
             size must be greater than 0
+        """
+
+    @property
+    def size(self):
+        return self.__size
+    """
+    Gets the value of a private instance
+    Returns:
+        the size of the square 
+    """
+
+    @property
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        """
+        sets the value of size
+        Args:
+            value(int): the value to set size to
         """
     def area(self):
         return self.__size * self.__size
