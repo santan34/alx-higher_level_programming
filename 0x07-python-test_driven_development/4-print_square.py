743f7b01a091f39e60cf6e3b94c75061769dea3d
St <takundaveni@gmail.com>
2024-01-31 07:54:14 -0800

    fix 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,14 +5,14 @@ prints a square of #'s
 
 
 def print_square(size):
+    """
+    prints a square of #'s
+    Args:
+        size(int): the dimension of the square
+    """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
         print("#"*size)
-    """
-    prints a square of #'s
-    Args:
-        size(int): the dimension of the square
-    """
