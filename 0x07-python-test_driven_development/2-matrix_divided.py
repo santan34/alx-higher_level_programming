3bfca36084db93b30a5d7c272e32f542a30587a3
St <takundaveni@gmail.com>
2024-01-31 07:30:47 -0800

    trial fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,8 +5,17 @@ Divides a matrix
 
 
 def matrix_divided(matrix, div):
+    """
+    a function that divides a list
+    the parameters can be ints or floats
+    Args:
+        matrix(ist): A list containing a list of integers or floats
+        div(int/float): A number to be used as the divisor
+        :Returns(list): a list of qoutiants
+    """
+
     length = len(matrix[0])
-    if type(div) is not int and type(div) is not float:
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -14,15 +23,8 @@ def matrix_divided(matrix, div):
         if len(row) != length:
             raise TypeError("Each row of the matrix must have the same size")
         for i in row:
-            if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    newlist = [[round(num/div, 2) for num in row] for row in matrix]
+            if not isinstance(i, int) and not isinstance(i, float):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+    newlist = [[round(num / div, 2) for num in row] for row in matrix]
     return newlist
-    """
-    a function that divides a list 
-    the parameters can be ints or floats
-    Args:
-        matrix(ist): A list containing a list of integers or floats
-        div(int/float): A number to be used as the divisor
-    :Returns(list): a list of qoutiants
-    """
