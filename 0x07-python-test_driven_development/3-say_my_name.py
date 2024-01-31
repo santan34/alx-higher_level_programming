6d184ad8c8f0b4b6213742b0ec4c4a6873a6ccb3
St <takundaveni@gmail.com>
2024-01-31 07:45:22 -0800

    3 fixe

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,11 +5,6 @@ prints out the full name of the person
 
 
 def say_my_name(first_name, last_name=""):
-    if type(first_name) is not str:
-        raise TypeError("first_name must be a string")
-    elif type(last_name) is not str:
-        raise TypeError("last_name must be a string")
-    print(f"My name is {first_name} {last_name}")
     """
     a function that prints out the first and last name of a person
     the function only takes strings as input
@@ -17,3 +12,9 @@ def say_my_name(first_name, last_name=""):
         first_name(str): the first name
         last_name(str): the last name
     """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
