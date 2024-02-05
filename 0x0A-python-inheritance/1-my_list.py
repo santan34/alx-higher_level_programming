d52873fe9ecc6a7bf0945a95ba985e8279b03161
St <takundaveni@gmail.com>
2024-02-05 15:36:21 -0800

    2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+"""
+firts inheritance
+"""
+
+
+class MyList(list):
+    """
+    A class that inherits from list
+    """
+    def print_sorted(self):
+        """
+        prinnts a sorted list
+        """
+        print(sorted(self))
