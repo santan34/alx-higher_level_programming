3369a908d66573a60e7a17be9071e18440e86411
St <takundaveni@gmail.com>
2024-02-06 14:24:43 -0800

    7

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,6 +4,7 @@ Saves object to a file
 """
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """
     saves a json object to a file
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Create object from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    loads an object from json file
+    :param filename: the file to load from
+    """
+    with open(filename, mode="r", encoding="UTF8") as f:
+        return json.loads(f.readlines())
