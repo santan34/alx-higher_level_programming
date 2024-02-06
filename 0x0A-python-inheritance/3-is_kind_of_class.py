7d27ff912d8e4a534b70e97b6da06f03be39e458
St <takundaveni@gmail.com>
2024-02-05 16:37:00 -0800

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+is kind of classs
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns true if obj is a mamber of a_class
+    :param obj:
+    :param a_class:
+    :return: True or False
+    """
+    return isinstance(obj, a_class)
