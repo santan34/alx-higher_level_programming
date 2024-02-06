7601c4a7896b73f94b9ba9b9ff6cf78320b22789
St <takundaveni@gmail.com>
2024-02-05 16:48:20 -0800

    4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+inherits from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns true if  the obj is a sub class of a_class
+    :param obj: the obj to examine
+    :param a_class: the class to be checked
+    :return: True or false
+    """
+    if obj is type(a_class):
+        return False
+    return (issubclass(obj, a_class))
