74e20810cb76a3994beff14f13888a6027a82063
St <takundaveni@gmail.com>
2024-02-05 17:39:56 -0800

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Geometry classs
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
