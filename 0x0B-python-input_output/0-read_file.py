7b08c287b316d96a4b8d171e1e4cf84b6106ca2e
St <takundaveni@gmail.com>
2024-02-06 13:19:23 -0800

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0.Read file
+"""
+
+def read_file(filename=""):
+    """
+    Read from a file
+    :param filename: the file to read
+    """
+    with open(filename, mode="r", encoding="UTF8") as file:
+        for line in file:
+            print(line, end=" ")
