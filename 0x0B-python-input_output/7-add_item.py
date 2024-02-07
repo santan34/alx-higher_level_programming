1bd83814154e811a1125519d82ceab93eb93363f
St <takundaveni@gmail.com>
2024-02-06 16:12:19 -0800

    8

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Create object from a JSON file
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+if __name__ == "__main__":
+    try:
+        item = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        item = []
+        item.extend(sys.argv[1:])
+    save_to_json_file(item, 'add_item.json')
