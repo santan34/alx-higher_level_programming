eea0e1e0a6324fc14040b69b0ca7b76b791d8a8a
St <takundaveni@gmail.com>
2024-04-04 15:34:15 -0700

    ?

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,17 @@
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = NULL;
+	listint_t *temp = NULL;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	temp = *head; 
+	while (number < temp->next->n)
+	{
+		temp = temp->next;
+	}
+
+	node->next = temp->next;
+	temp->next = node;
+}
