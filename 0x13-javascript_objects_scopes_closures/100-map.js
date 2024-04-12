535a32fab279d6288fe0bbc21be64d85181b4050
St <takundaveni@gmail.com>
2024-04-12 09:05:15 -0700

    crack

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const list = require('100-data.js').list;
-const nlist = list.map((num, i) => num * i);
+let nlist = list.map((num, i) => num * i );
 console.log(list);
 console.log(nlist);
