09638686b2ab006c8bd63665198f3efc0e7fbc52
St <takundaveni@gmail.com>
2024-04-14 16:43:49 -0700

    template done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,9 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * from hbtn_0e_0_usa ORDER BY id ASC")
-    states = cur.fetchall()
-    for i in range(states):
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    stat = cur.fetchall()
+    for i in stat:
         print(i)
     cur.close()
     db.close()
