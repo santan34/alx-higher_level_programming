aa0dc02adfa7513fc5ee64bde35b308cba8086ec
St <takundaveni@gmail.com>
2024-04-14 16:29:32 -0700

    pep

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user = argv[1], passwd = argv[2], db = argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * from hbtn_0e_0_usa ORDER BY id ASC")
     states = cur.fetchall()
@@ -13,4 +13,3 @@ if __name__ == "__main__":
         print(i)
     cur.close()
     db.close()
-
