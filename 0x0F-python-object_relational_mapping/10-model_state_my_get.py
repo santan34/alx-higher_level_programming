1ba5a1423af461d2ce9dff60ef8792c3cd2271c1
St <takundaveni@gmail.com>
2024-04-14 19:42:28 -0700

    10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""intro to alx sql alchemny"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    db_url = "mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Sasho = sessionmaker(bind=engine)
+    session = Sasho()
+    for star in session.query(State).order_by(State.id):
+        if argv[4] == star.name:
+            print("{}: {}".format(star.id, star.name))
