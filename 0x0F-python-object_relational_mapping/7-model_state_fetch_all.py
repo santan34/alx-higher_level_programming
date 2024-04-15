f319710baa16be369b9cdacdd30b6a48f5da5ae3
St <takundaveni@gmail.com>
2024-04-14 19:21:09 -0700

    77

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     engine = create_engine(db_url, pool_pre_ping=True)
     Sasho = sessionmaker(bind=engine)
     session = Sasho()
-    for state in session.query(State).order(State.id):
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
