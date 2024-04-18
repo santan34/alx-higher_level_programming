13dac3d3114a9afb644f25364ed8b932301d5ffa
St <takundaveni@gmail.com>
2024-04-18 03:48:40 -0700

    fix pep8 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-lists all states with a name starting with N (upper N) from the database hbtn_0e_0_us
+lists all states with a name starting wit
 """
 import MySQLdb
 from sys import argv
