fb3981130d6bdeee02c5cec53fc66f5bdf567520
St <takundaveni@gmail.com>
2024-04-18 08:19:53 -0700

    trial 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""changes the name based on index"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    db = f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+    engine = create_engine(db, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    sasho = Session()
+    
+    state = sasho.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    sasho.commit()
