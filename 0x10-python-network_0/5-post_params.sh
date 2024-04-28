0368901d6aeffcefb229deb36805c9abfe52e4fe
St <takundaveni@gmail.com>
2024-04-28 03:58:34 -0700

    5555

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Post with params
-curl -sX POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
