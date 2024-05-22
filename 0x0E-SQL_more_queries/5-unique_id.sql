c4dc767811e6d198f3ff2993e7453482607fdca2
root <takundaveni@gmail.com>
2024-05-22 12:04:18 -0700

    55

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 --creates table
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1, UNIQUE(id), name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `unique_id`(`id` INT DEFAULT 1, UNIQUE(`id`), `name` VARCHAR(256));
