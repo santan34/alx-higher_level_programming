7d4bae69638c43c15ffd1d3c44c05d5ccd432a64
St <takundaveni@gmail.com>
2024-06-08 00:41:32 +0200

    i d k

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""more requests"""
-
+"""git tish"""
 import requests
-import sys
-
-r = requests.get(
-    'https://api.github.com/user',
-    auth=(
-        sys.argv[1],
-        sys.argv[2]))
-print(r.json().get('id'))
+from sys import argv
+try:
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json().get('id'))
+except BaseException:
+    print("None")
