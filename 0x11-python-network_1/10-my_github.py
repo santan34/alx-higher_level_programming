51ff87a97322cd6aec165a519c38f0cb3c288394
St <takundaveni@gmail.com>
2024-06-08 00:34:23 +0200

    testt

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,8 +2,5 @@
 """more requests"""
 import requests
 import sys
-try:
-    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    print(r.json().get('id'))
-except BaseException:
-    print("None")
+r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+print(r.json().get('id'))
