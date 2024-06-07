b1fb52e1f9ce9a8bef06615859c8eaf73d7cb71e
St <takundaveni@gmail.com>
2024-06-08 00:37:09 +0200

    test 2

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,5 +2,9 @@
 """more requests"""
 import requests
 import sys
-r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+r = requests.get(
+    'https://api.github.com/user',
+    auth=(
+        sys.argv[1],
+        sys.argv[2]))
 print(r.json().get('id'))
