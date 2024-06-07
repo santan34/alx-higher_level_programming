043228fe4772700191859e92fa34af70636ea40c
St <takundaveni@gmail.com>
2024-06-07 18:35:45 +0200

    4

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""url lib 3"""
+
+import sys
+import urllib.error
+import urllib.request
+
+
+def reqmail(url):
+    """the function reqs a url handles errors"""
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            page = response.read().decode('utf-8')
+            print(page)
+    except urllib.error.HTTPError as e:
+        print(f"urllib.error: {e.code}")
+
+
+if __name__ == "__main__":
+    reqmail(sys.argv[1])
